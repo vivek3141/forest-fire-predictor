be47f3653be0c104de73aeff9383ad00280315e5
Vivek Verma <vivnps.verma@gmail.com>
2018-11-17 23:14:38 -0800

    Fixing Files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request, make_response
-import pickle
+import tensorflow as tf
 import numpy as np
 import os
 import datetime
@@ -8,6 +8,7 @@ import json
 import urllib.request
 from src.fwi import *
 from src.test import build_model
+from keras import backend as K
 
 app = Flask(__name__)
 
@@ -21,21 +22,34 @@ def home():
 
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
-    c = list(map(float, request.args.get('c')[1:-1].split(",")))
-    now = datetime.datetime.now()
-    month = now.month
-    day = now.day
+    """
+    Y is Latitude, X is Longitude
+    :return:
+    """
     x1 = -7.182288
     x2 = -6.515598
     y2 = 41.992531
     y1 = 41.732416
-    X = int(math.fabs((9 * (c[0] - x1)) / (x2 - x1)))
-    Y = int(math.fabs((9 * (c[1] - y1)) / (y2 - y1)))
+    dates = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    c = list(map(float, request.args.get('c')[1:-1].split(",")))
+    now = datetime.datetime.now()
+    month = now.month
+    day = dates.index(now.strftime("%A")) + 1
+    if not (y1 <= c[0] <= y2):
+        return "Outside Bounds"
+    if not (x1 <= c[1] <= x2):
+        return "Outside Bounds"
+    x_p = (x2 - x1) / 9
+    y_p = (y2 - y1) / 9
+    delta_x = c[0] - y1
+    delta_y = c[1] - x1
+    X = int(delta_x/x_p)
+    Y = int(delta_y/y_p)
+    print(X, Y)
     url = "http://api.openweathermap.org/data/2.5/weather?lat={}&" \
           "lon={}&appid=997248ab2a9c56c05cf48c93efca9b27".format(c[0], c[1])
 
     j = json.load(urllib.request.urlopen(url))
-    print(j)
     try:
         temp = j['main']['temp'] - 273.15
         wind = j['wind']['speed'] * 3.6
@@ -50,13 +64,15 @@ def predict():
     data = str(X) + "," + str(Y) + "," + str(month) + "," + str(day) + "," + str(ffmc) + "," + str(
         dmc) + "," + str(dc) + "," + str(isi) + "," + str(temp) + "," + str(rh) + "," + str(wind) + ',' + str(
         rain)
+    print(data)
     model = build_model()
     model.load_weights("./src/model_weights.h5")
     data = np.array(list(map(float, data.split(",")))).reshape(-1, 1).T
     predicted = model.predict(data)
-    return str(predicted[0][0])
+    K.clear_session()
+    return str(math.fabs(predicted[0][0]))
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=False)
